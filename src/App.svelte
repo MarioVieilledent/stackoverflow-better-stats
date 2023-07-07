1dc305606d655058a232c238ce3295b93d7ba3ae
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-07-07 14:20:32 +0200

    More and better graphs

--- src/App.svelte
+++ src/App.svelte
@@ -8,24 +8,28 @@
       formula: "% of respondents who want to use it",
       indexInArray: 0,
     },
+    /*
     {
       title: "Average focus on desired",
       desc: "First quartile",
       formula: "(3*Desired + 1*Admired) / 4",
       indexInArray: 2,
     },
+    */
     {
       title: "Average",
       desc: "Point in between Desired and Admired",
       formula: "(Desired + Admired) / 2",
       indexInArray: 3,
     },
+    /*
     {
       title: "Average focus on admired",
       desc: "Third quartile",
       formula: "(1*Desired + 3*Admired) / 4",
       indexInArray: 4,
     },
+    */
     {
       title: "Admired",
       desc: "From StackOverflow 2023's survey",
@@ -34,36 +38,39 @@
     },
     {
       title: "Underestimated",
-      desc: "From StackOverflow 2023's survey",
+      desc: "Width between Desired and Admired",
       formula: "Admired - Desired",
       indexInArray: 5,
     },
   ];
 
-  // Adds averages focus on desired on index 2
-  for (const [lang, vals] of Object.entries(stats)) {
-    vals.push((vals[0] * 3 + vals[1]) / 4.0);
+  // Calculate stats for one graph
+  function calcStats(graph: any): void {
+    // Adds averages focus on desired on index 2
+    for (const [lang, vals] of Object.entries(graph)) {
+      (vals as any).push((vals[0] * 3 + vals[1]) / 4.0);
+    }
+    // Adds averages  index 3
+    for (const [lang, vals] of Object.entries(graph)) {
+      (vals as any).push((vals[0] + vals[1]) / 2.0);
+    }
+    // Adds averages focus on admired  index 4
+    for (const [lang, vals] of Object.entries(graph)) {
+      (vals as any).push((vals[0] + vals[1] * 3) / 4.0);
+    }
+    // Adds span on index 5
+    for (const [lang, vals] of Object.entries(graph)) {
+      (vals as any).push(vals[1] - vals[0]);
+    }
   }
 
-  // Adds averages  index 3
-  for (const [lang, vals] of Object.entries(stats)) {
-    vals.push((vals[0] + vals[1]) / 2.0);
-  }
-
-  // Adds averages focus on admired  index 4
-  for (const [lang, vals] of Object.entries(stats)) {
-    vals.push((vals[0] + vals[1] * 3) / 4.0);
-  }
-
-  // Adds span on index 5
-  for (const [lang, vals] of Object.entries(stats)) {
-    vals.push(vals[1] - vals[0]);
-  }
+  // Calculate all stats
+  Object.values(stats).forEach(graph => calcStats(graph));
 
   let currSpot = "";
   let currLang = "";
 
-  function mouseEnter(spot, lang) {
+  function mouseEnter(spot: string, lang: string): void {
     currSpot = spot;
     currLang = lang;
   }
@@ -71,23 +78,29 @@
 
 <main>
   <div class="top">
-    {#each display as listToDisplay}
-      <div class="list">
-        <h2>{listToDisplay.title}</h2>
-        <span class="desc">{listToDisplay.desc}</span>
-        <span class="formula">{listToDisplay.formula}</span>
-        {#each Object.entries(stats).sort((a, b) => b[1][listToDisplay.indexInArray] - a[1][listToDisplay.indexInArray]) as [lang, vals], index}
-          <div
-            class={"lang" +
-              (lang === currLang ? " selected" : "") +
-              " tier" +
-              Math.floor(index / 10.0 + 1.0)}
-            on:mouseenter={() => mouseEnter("Desired", lang)}
-            on:mouseleave={() => mouseEnter("", "")}
-          >
-            <span>{index + 1}&#41;</span>
-            <span>{lang}</span>
-            <span>{vals[listToDisplay.indexInArray].toFixed(2)}&#37;</span>
+    {#each Object.entries(stats) as [graphName, graphStats]}
+    {@const numberOfItemInTier = Object.keys(graphStats).length / 5.0}
+      <h2>{graphName}</h2>
+      <div class="graph">
+        {#each display as listToDisplay}
+          <div class="list">
+            <h2>{listToDisplay.title}</h2>
+            <span class="desc">{listToDisplay.desc}</span>
+            <span class="formula">{listToDisplay.formula}</span>
+            {#each Object.entries(graphStats).sort((a, b) => b[1][listToDisplay.indexInArray] - a[1][listToDisplay.indexInArray]) as [lang, vals], index}
+              <div
+                class={"lang" +
+                  (lang === currLang ? " selected" : "") +
+                  " tier" +
+                  Math.floor(index / numberOfItemInTier + 1.0)}
+                on:mouseenter={() => mouseEnter("Desired", lang)}
+                on:mouseleave={() => mouseEnter("", "")}
+              >
+                <span>{index + 1}&#41;</span>
+                <span>{lang}</span>
+                <span>{vals[listToDisplay.indexInArray].toFixed(2)}&#37;</span>
+              </div>
+            {/each}
           </div>
         {/each}
       </div>
@@ -130,38 +143,47 @@
 
     .top {
       display: flex;
+      flex-direction: column;
       margin-bottom: 24px;
       justify-content: center;
+      align-items: center;
 
-      .list {
+      .graph {
         display: flex;
-        flex-direction: column;
-        margin-right: 36px;
+        margin-bottom: 24px;
+        justify-content: center;
 
-        h2 {
-          margin: 12px 0px 6px 0px;
-          font-size: 20px;
-        }
-
-        .desc {
-          margin-bottom: 6px;
-          font-size: 14px;
-          white-space: nowrap;
-        }
-
-        .formula {
-          color: #aaa;
-          margin-bottom: 6px;
-          font-size: 14px;
-          white-space: nowrap;
-        }
-
-        .lang {
+        .list {
           display: flex;
-          padding-left: 3px;
+          flex-direction: column;
+          margin-right: 36px;
 
-          span {
-            margin-right: 12px;
+          h2 {
+            margin: 12px 0px 6px 0px;
+            font-size: 20px;
+          }
+
+          .desc {
+            margin-bottom: 6px;
+            font-size: 14px;
+            white-space: nowrap;
+          }
+
+          .formula {
+            color: #aaa;
+            margin-bottom: 6px;
+            font-size: 14px;
+            white-space: nowrap;
+          }
+
+          .lang {
+            display: flex;
+            padding-left: 3px;
+
+            span {
+              margin-right: 12px;
+              font-size: 14px;
+            }
           }
         }
       }
